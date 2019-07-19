61e41ef948cd47f4cdac65a0bda0ee3689cbaebe
FriscoDY <cday02@gmail.com>
2019-07-18 20:07:40 -0500

    fixed initial inventory issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./pages/Header"
+import Inventory from "./pages/Inventory/Inventory"
 
 
 function App() {
@@ -8,9 +9,9 @@ function App() {
     <Router>
       <div>
       <Route exact path="/" component={ Header } />
+      <Route exact path="/pages/Inventory/Inventory.js" component= { Inventory } />
       {/* <Route exact path="/About" component= { About } />
       <Route exact path="/Contact" component= { Contact } />
-      <Route exact path="/Inventory" component= { Inventory } />
       <Route exact path="/Featured" component= { Featured } /> */}
       </div>
     </Router>
--- client/src/pages/Inventory/Inventory.js
+++ client/src/pages/Inventory/Inventory.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
-
+import API from "../../utils/API"
+class Inventory extends Component {
+state={
+        items:[],
+    }
 componentDidMount() {
-    this.loadItems();
-  }
+    this.loadItems()
+}
 
   // Loads all Items  and sets them to this.state.items
   loadItems = () => {
     API.getItems()
       .then(res =>
-        this.setState({ items: res.data, type: "", rarity: "" })
+        this.setState({ items: res.data })
       )
       .catch(err => console.log(err));
   };
-
+  
+  render(){
+    const {type, rarity}=this.state.items
+    return(
+    <div>
+     {type} 
+     {rarity}  
+    </div>
+    )
+  }
+}
 export default Inventory
